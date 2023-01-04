56c3d762b9bb54a12b7efda5671fa786744d492c
lizuAg <wndms058@gmail.com>
2023-01-05 00:18:36 +0900

    [setting/#0] 프로젝트 초기 세팅

---
+++ src/main/java/yeonjeans/saera/SaeraApplication.java
@@ -0,0 +1,13 @@
+package yeonjeans.saera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaeraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaeraApplication.class, args);
+	}
+
+}
