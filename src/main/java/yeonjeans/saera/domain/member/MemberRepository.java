cacb8fe43b6876df5152669f1038e42c2c55220d
lizuAg <wndms058@gmail.com>
2023-02-20 03:31:43 +0900

    [Test]add findMemberWithPlaform Test

--- src/main/java/yeonjeans/saera/domain/member/MemberRepository.java
+++ src/main/java/yeonjeans/saera/domain/member/MemberRepository.java
@@ -3,6 +3,7 @@ package yeonjeans.saera.domain.member;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -10,9 +11,9 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByEmail(String email);
 
-    @EntityGraph(attributePaths = {"roleSet"}, type = EntityGraph.EntityGraphType.LOAD)
-    @Query("select m from Member m where m.platform = :platform and m.email = :email")
-    Optional<Member> findByEmail(String email, Platform platform);
+    @Query("select m from Member m where  m.email = :email and m.platform = :platform")
+    Optional<Member> findByEmail(@Param("email") String email,@Param("platform") Platform platform);
 
+    @EntityGraph(attributePaths = {"roleSet"}, type = EntityGraph.EntityGraphType.LOAD)
     Optional<Member> findById(Long id);
 }
--- src/test/java/yeonjeans/saera/domain/member/MemberRepositoryTest.java
+++ src/test/java/yeonjeans/saera/domain/member/MemberRepositoryTest.java
@@ -1,4 +1,30 @@
 package yeonjeans.saera.domain.member;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
 public class MemberRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Transactional
+    @Test
+    public void findMemberWithPlatformTest(){
+        Member member = Member.builder()
+                .profile("test")
+                .platform(Platform.GOOGLE)
+                .email("test")
+                .nickname("testuser1")
+                .build();
+        member.addMemberRole(MemberRole.USER);
+        memberRepository.save(member);
+
+        Member result = memberRepository.findByEmail("test", Platform.GOOGLE).get();
+
+        Assertions.assertEquals(member, result);
+    }
 }
