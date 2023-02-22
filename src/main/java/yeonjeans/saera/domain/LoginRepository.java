76229fe4339418670441399979eef2ca04c3ad28
lizuAg <wndms058@gmail.com>
2023-02-22 14:46:45 +0900

    [Fix] rename wrong query method: member_Id to memberId

--- src/main/java/yeonjeans/saera/domain/LoginRepository.java
+++ src/main/java/yeonjeans/saera/domain/LoginRepository.java
@@ -1,10 +1,15 @@
 package yeonjeans.saera.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface LoginRepository extends JpaRepository<Login, Long> {
 
-    public Optional<Login> findByMember_Id(Long MemberId);
+    public Optional<Login> findByMemberId(Long MemberId);
+
+    @Query("select l from Login l where l.RefreshToken = :token")
+    public Optional<Login> findByRefreshToken(@Param("token")String refreshToken);
 }
