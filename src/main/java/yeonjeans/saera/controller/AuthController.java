0f5a180e9794b94dea6a9bbf6fb4b900c8a6404c
lizuAg <wndms058@gmail.com>
2023-02-24 01:32:35 +0900

    [Chore] add swagger-ui description in AuthController

--- src/main/java/yeonjeans/saera/controller/AuthController.java
+++ src/main/java/yeonjeans/saera/controller/AuthController.java
@@ -1,5 +1,10 @@
 package yeonjeans.saera.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
@@ -8,9 +13,11 @@ import yeonjeans.saera.Service.MemberService;
 import yeonjeans.saera.domain.member.Member;
 import yeonjeans.saera.domain.member.MemberRepository;
 import yeonjeans.saera.domain.member.Platform;
+import yeonjeans.saera.dto.StatementResponseDto;
 import yeonjeans.saera.dto.oauth.GoogleUser;
 
 import yeonjeans.saera.dto.TokenResponseDto;
+import yeonjeans.saera.exception.ErrorResponse;
 import yeonjeans.saera.security.service.OAuthService;
 
 @Log4j2
@@ -22,6 +29,11 @@ public class AuthController {
     private final MemberService memberService;
     private final MemberRepository memberRepository;
 
+    @Operation(summary = "토큰 요청", description = "구글 Server Auth Code를 통해 유저 정보를 받아오고, 토큰 발급합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "조회 성공", content = @Content(schema = @Schema(implementation = TokenResponseDto.class)))
+            }
+    )
     @GetMapping(value = "/auth/google/callback")
     public ResponseEntity<?> callback(
             @RequestParam(name = "code") String code) {
