42707900ac77e84f61e52b5831c88f55d0a31491
lizuAg <wndms058@gmail.com>
2023-02-27 18:42:24 +0900

    [Chore] 사용하지 않는 import문 삭제

--- src/main/java/yeonjeans/saera/dto/PracticedRequestDto.java
+++ src/main/java/yeonjeans/saera/dto/PracticedRequestDto.java
@@ -3,8 +3,6 @@ package yeonjeans.saera.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
